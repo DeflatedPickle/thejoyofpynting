62ecae77c25b9f85bac659703ef1a2c108a928c6
Dibbo <DeflatedPickle@gmail.com>
2017-05-16 01:42:38 +0100

    Added local versions of the images

--- thejoyofpynting_test.py
+++ thejoyofpynting_test.py
@@ -9,7 +9,11 @@ import thejoyofpynting
 
 image_location = urllib.request.urlopen(urllib.request.Request("https://ih0.redbubble.net/image.353370368.9497/flat,800x800,075,f.u1.jpg"))
 
-# image = thejoyofpynting.paint_a_picture(image_location)
-# image = thejoyofpynting.paint_a_picture(image_location, 1)
-image = thejoyofpynting.paint_a_picture(image_location, 2)
-Image.Image.show(image)
+image1 = thejoyofpynting.paint_a_picture(image_location, 0)
+Image.Image.show(image1)
+
+image2 = thejoyofpynting.paint_a_picture(image_location, 1)
+Image.Image.show(image2)
+
+image3 = thejoyofpynting.paint_a_picture(image_location, 2)
+Image.Image.show(image3)
