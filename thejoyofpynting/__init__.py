323dfb8a1358b5ec7d9c2c337ece4b5d51ce2b4b
Dibbo <DeflatedPickle@gmail.com>
2017-05-19 22:52:09 +0100

    Added more templates and moved some things around

--- thejoyofpynting/__init__.py
+++ thejoyofpynting/__init__.py
@@ -2,10 +2,7 @@
 # -*- coding: utf-8 -*-
 """"""
 
-import os
-import urllib.request
-
-from PIL import Image
+from .paintapicture import paint_a_picture
 
 __title__ = "thejoyofpynting"
 
@@ -14,49 +11,7 @@ __copyright__ = "Copyright (c) 2017 Dibbo"
 __credits__ = ["DeflatedPickle/Dibbo"]
 
 __license__ = "MIT"
-__version__ = "1.3.0"
+__version__ = "1.4.0"
 __maintainer__ = "DeflatedPickle/Dibbo"
 __email__ = "DeflatedPickle@gmail.com"
 __status__ = "Development"
-
-bob_types = {
-    0: {
-        "image": os.path.join(os.path.dirname(__file__), "1.png"), # urllib.request.urlopen(urllib.request.Request("http://i.imgur.com/nVMsfWs.png")),
-        "place_x": 20,
-        "place_y": 60,
-        "size_x": 360,
-        "size_y": 280
-    },
-    1: {
-        "image": os.path.join(os.path.dirname(__file__), "2.png"), # urllib.request.urlopen(urllib.request.Request("http://i.imgur.com/hCwwRCJ.png")),
-        "place_x": 450,
-        "place_y": 35,
-        "size_x": 260,
-        "size_y": 305
-    },
-    2: {
-        "image": os.path.join(os.path.dirname(__file__), "3.png"), # urllib.request.urlopen(urllib.request.Request("http://i.imgur.com/JFNOMLL.png")),
-        "place_x": 797,
-        "place_y": 15,
-        "size_x": 225,
-        "size_y": 450
-    }
-}
-
-
-def paint_a_picture(file: str="", bobtype: int=0):
-    image = Image.open(file)
-    image = image.convert("RGBA")
-    image = image.resize((bob_types[bobtype]["size_x"], bob_types[bobtype]["size_y"]))
-
-    bob = Image.open(bob_types[bobtype]["image"])
-    bob = bob.convert("RGBA")
-
-    final = Image.new("RGBA", bob.size)
-    final.paste(image, (bob_types[bobtype]["place_x"], bob_types[bobtype]["place_y"]), image)
-    final.paste(bob, (0, 0), bob)
-    # final = Image.alpha_composite(final, image)
-    # final = Image.alpha_composite(final, bob)
-
-    # final.show()
-    return final
--- thejoyofpynting_test.py
+++ thejoyofpynting_test.py
@@ -2,22 +2,31 @@
 # -*- coding: utf-8 -*-
 """"""
 
-import urllib.request
-
 from PIL import Image
 import thejoyofpynting
 
-image_location = urllib.request.urlopen(urllib.request.Request("https://ih0.redbubble.net/image.353370368.9497/flat,800x800,075,f.u1.jpg"))
-local_image_location = ""
+local_image_location = ".png"  # Provide a local image
 
-image1 = thejoyofpynting.paint_a_picture(image_location, 0)
-# Image.Image.show(image1)
-image1.save("1-edit.png")
+image1 = thejoyofpynting.paint_a_picture(local_image_location, 0)
+Image.Image.show(image1)
+# image1.save("1-edit.png")
 
-image2 = thejoyofpynting.paint_a_picture(image_location, 1)
-# Image.Image.show(image2)
-image2.save("2-edit.png")
+image2 = thejoyofpynting.paint_a_picture(local_image_location, 1)
+Image.Image.show(image2)
+# image2.save("2-edit.png")
 
-image3 = thejoyofpynting.paint_a_picture(image_location, 2)
-# Image.Image.show(image3)
-image3.save("3-edit.png")
+image3 = thejoyofpynting.paint_a_picture(local_image_location, 2)
+Image.Image.show(image3)
+# image3.save("3-edit.png")
+
+image4 = thejoyofpynting.paint_a_picture(local_image_location, 3)
+Image.Image.show(image4)
+# image4.save("4-edit.png")
+
+image5 = thejoyofpynting.paint_a_picture(local_image_location, 4)
+Image.Image.show(image5)
+# image5.save("5-edit.png")
+
+image6 = thejoyofpynting.paint_a_picture(local_image_location, 5)
+Image.Image.show(image6)
+# image6.save("6-edit.png")
