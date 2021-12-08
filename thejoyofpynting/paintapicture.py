129448b8453441acc320e9e2dfcf1190614d6255
DeflatedPickle <deflatedpickle@gmail.com>
2021-12-08 09:00:08 +0000

    Made the function return a BytesIO object

--- thejoyofpynting/paintapicture.py
+++ thejoyofpynting/paintapicture.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """"""
+from io import BytesIO
 
 from PIL import Image
 
 from .bobtypes import bob_types
 
 
-def paint_a_picture(file: str="", bobtype: int=0):
+def paint_a_picture(file: BytesIO, bobtype: int = 0):
     image = Image.open(file)
     image = image.convert("RGBA")
     image = image.resize((bob_types[bobtype]["size_x"], bob_types[bobtype]["size_y"]))
@@ -18,8 +19,10 @@ def paint_a_picture(file: str="", bobtype: int=0):
     final = Image.new("RGBA", bob.size)
     final.paste(image, (bob_types[bobtype]["place_x"], bob_types[bobtype]["place_y"]), image)
     final.paste(bob, (0, 0), bob)
-    # final = Image.alpha_composite(final, image)
-    # final = Image.alpha_composite(final, bob)
 
     # final.show()
-    return final
+
+    bio = BytesIO()
+    final.save(bio, "PNG")
+
+    return bio
